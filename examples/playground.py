5e1629b046b0b412f0a5ea7cd65ebdbd865b9cfa
shuyao <shuyao.guo@tu-berlin.de>
2024-03-12 17:47:41 +0100

    Updated examples

---
+++ examples/playground.py
@@ -0,0 +1,30 @@
+import os
+
+import eflips.eval.input.prepare as input_prepare
+import eflips.eval.output.prepare as output_prepare
+import eflips.eval.input.visualize as input_visualize
+import eflips.eval.output.visualize as output_visualize
+
+import plotly.express as px  # type: ignore
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    engine = create_engine(os.environ.get("DATABASE_URL"))
+    session = Session(engine)
+    SCENARIO_ID = 8
+
+    prepared_data = output_prepare.departure_arrival_soc(
+        SCENARIO_ID, session
+    )
+    fig = output_visualize.departure_arrival_soc(prepared_data)
+    fig.show()
+
+    prepared_data = input_prepare.rotation_info(SCENARIO_ID, session)
+    fig = input_visualize.rotation_info(prepared_data)
+    fig.show()
+
+    prepared_data = output_prepare.depot_event(SCENARIO_ID, session)
+
+    fig = output_visualize.depot_event(prepared_data)
+    fig.show()
