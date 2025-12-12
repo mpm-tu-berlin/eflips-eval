e10d05f778e756ec8035df3e37da1d53940b4be2
Lu Heide <ludger.heide@lhtechnologies.de>
2025-12-12 22:07:15 +0100

    Bump Version

--- doc/conf.py
+++ doc/conf.py
@@ -9,7 +9,7 @@
 project = "eflips-eval"
 copyright = "2024, Technische Universität Berlin"
 author = "Ludger Heide"
-release = "1.7.8"
+release = "1.8.0"
 
 
 # -- General configuration ---------------------------------------------------
